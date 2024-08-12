96316ef0e079aea7d247531049fcc952c7f4bc28
Alex Sokolek <asokolek@verizon.net>
2024-08-12 18:15:09 -0400

    Sort of working

--- Newton/Newton.cpp
+++ Newton/Newton.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
+#include <iomanip>
 
 int main()
 {
 	double x0 = 0.9;
-	double epsilon = 0.00000000000001;
+	double epsilon = 1.0e-14;
 	#define f(x) (pow(4.0, x) + pow(6.0, x) - 9.0)
-	double x1, y1, m, b;
+	double x1, y0, y1, m;
+	char chr;
+	y0 = f(x0);
 
-	while (xleft * (1.0 + epsilon) < xright)
+	for (;;)
 	{
-		yleft = f(xleft);
-		m = (f(xleft * (1.0 + epsilon)) - yleft) / (xleft * (1.0 + epsilon) - xleft);
-		b = yleft - m * xleft;
-		x = -b / m;
-		y = f(x);
-
+		m = (f(x0 * (1.0 + 1.0e-6)) - f(x0)) / (x0 * (1.0 + 1.0e-6) - x0);
+		x1 = x0 - y0 / m;
+		y1 = f(x0);
+		std::cout << std::scientific << std::setprecision(14) << std::showpos;
+		std::cout << "X0: " << x0 << "     Y0: " << y0 << "     M: " << m << "     X1 : " << x1 << "     Y1 : " << y1 << std::endl;
+		if (abs(x0 - x1) < 0.0001) break;
+		x0 = x1;
+		y0 = y1;
 	}
 }
