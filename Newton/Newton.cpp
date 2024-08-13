e158f2ba9a78ecad3cbefa4a6395252236b4cc01
Alex Sokolek <asokolek@verizon.net>
2024-08-13 02:34:16 -0400

    First release - 1.0.0.1

--- Newton/Newton.cpp
+++ Newton/Newton.cpp
@@ -1,8 +1,49 @@
 // newton.cpp - Finds root of function by using the Newton–Raphson method.
 //
-// Currently configured to find the root of 4^X + 6^X - 9.
+// Currently configured to find the root of 4^X + 6^X -9 = 0.
 // To configure for other functions, see the two defines.
 // Note that in addition to the main function, you need the deriviative.
+//
+// Only finds one root.This program was written to solve a FaceBook math
+// problem, namely solve for x where 4 ^ x + 6 ^ x = 9.
+// 
+// Microsoft Visual Studio 2022 Community Edition 64 Bit 17.10.5
+//
+// Alex Sokolek, Version 1.0.0.1, Copyright (c) August 13, 2024
+// 
+/////////////////////////////////////////////////////////////////////////
+//
+// BSD 3 - Clause License
+//
+// Copyright(c) 2024, Alex Sokolek
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met :
+// 
+// 1. Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and /or other materials provided with the distribution.
+// 
+// 3. Neither the name of the copyright holder nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+///////////////////////////////////////////////////////////////////////
 
 #include <iostream>
 #include <iomanip>
@@ -21,18 +62,38 @@ int main()
 
 	for (;;)
 	{
+		// Calculate slope at x0.
 		m = dydx(x0);
+		if (m == 0.0)
+		{
+			std::cout << "ERROR: dydx(" << x0 << ") is zero." << std::endl;
+			break;
+		}
+		
+		// Calculate xn+1.
 		x1 = x0 - y0 / m;
+
+		// Not needed for the algorithm. Included for diagnostic purposes.
 		y1 = f(x1);
+
+		// Output state.
 		std::cout <<    "X0: "  << x0;
 		std::cout << "   Y0: "  << y0;
 		std::cout << "   M: "   << m;
 		std::cout << "   X1 : " << x1;
 		std::cout << "   Y1 : " << y1;
 		std::cout << std::endl;
+
+		// Stop when desired precision is reached.
 		if (abs(x0 - x1) < epsilon) break;
+
+		// Iterate, shifting x1,y1 to x0,y0.
 		x0 = x1;
 		y0 = y1;
 	}
+
+	// Display root.
+	std::cout << std::endl << "Found one (only) root at " << x0 << std::endl;
+
 	return 0;
 }
---
+++ README.md
@@ -0,0 +1,8 @@
+newton.cpp - Finds root of function by using the Newton–Raphson method.
+
+Currently configured to find the root of 4^X + 6^X - 9.
+To configure for other functions, see the two defines.
+Note that in addition to the main function, you need the derivative.
+
+Only finds one root. This program was written to solve a FaceBook math
+problem, namely solve for x where 4^x +6^x = 9.
