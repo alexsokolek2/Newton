6409d9d1bbf3067d4434a2b8ec1e449e67cba86d
Alex Sokolek <asokolek@verizon.net>
2024-08-12 22:12:01 -0400

    Working code

--- Newton/Newton.cpp
+++ Newton/Newton.cpp
@@ -1,15 +1,20 @@
+// newton.cpp - Finds root of function by using the Newton–Raphson method.
+//
+// Currently configured to find the root of 4^X + 6^X - 9.
+// To configure for other functions, see the two defines.
+// Note that in addition to the main function, you need the deriviative.
+
 #include <iostream>
 #include <iomanip>
-#include <math.h>
 
 int main()
 {
-	double x0 = 0.9;
-	double epsilon = 1.0e-14;
 	#define f(x) (pow(4.0, x) + pow(6.0, x) - 9.0)
 	#define dydx(x) (pow(4.0, x) * log(4.0) + pow(6.0, x) * log(6.0))
-	double x1, y0, y1, m;
-	char chr;
+	double x0, y0, x1, y1, m;
+	double epsilon = 1.0e-14;
+	
+	x0 = 0.9; // Initial guess
 	y0 = f(x0);
 
 	std::cout << std::scientific << std::setprecision(14) << std::showpos;
@@ -18,14 +23,14 @@ int main()
 	{
 		m = dydx(x0);
 		x1 = x0 - y0 / m;
-		y1 = f(x0);
+		y1 = f(x1);
 		std::cout <<    "X0: "  << x0;
 		std::cout << "   Y0: "  << y0;
 		std::cout << "   M: "   << m;
 		std::cout << "   X1 : " << x1;
 		std::cout << "   Y1 : " << y1;
 		std::cout << std::endl;
-		if (abs(x0 - x1) < 0.0001) break;
+		if (abs(x0 - x1) < epsilon) break;
 		x0 = x1;
 		y0 = y1;
 	}
